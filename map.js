3ee33e3f22d5ff64981714973f622637b28caab3
unknown <yannis.almanric@gmail.com>
2021-04-02 17:50:18 +0200

    POO with class Map + WIP checkSiblingCells

--- map.js
+++ map.js
@@ -5,6 +5,7 @@
  class Map {
     constructor(settings) {
         this.registerSettings(settings);
+        this.boot();
     }
   
     registerSettings(userSettings) {
@@ -12,9 +13,244 @@
           columnsNumber: 10,
           rowsNumber: 10,
           percentageDisabledCells: 15,
-          weaponsCount : 5
+          weaponsCount : 5,
+          el: document.querySelector('table'),
       };
       Object.assign(this, {...defaultSettings, ...userSettings});
+    }
+
+    boot() {
+        this.generateMap();
+        this.generateWalls();
+        this.generateWeapons();
+        this.placePlayer(players[0]);
+        this.placePlayer(players[1]);
+        this.checkSiblingsCells();
+        this.printMap();
+    }
+
+    generateMap() {
+        this.map = [];
+        for (let y = 0; y < this.rowsNumber; y++) {
+            this.map[y] = [];
+            for (let x = 0; x < this.columnsNumber; x++) {
+                this.map[y][x] = {x, y};
+            }
+        }
+    }
+
+    generateWalls() {
+        const wallsToBuild = Math.floor((this.rowsNumber * this.columnsNumber) * (this.percentageDisabledCells / 100));
+        for (let i = 0; i < wallsToBuild; i++) {
+            const cell = this.getAvailableRandomCell();
+            this.map[cell.y][cell.x].wall = true;
+        }
+    }
+    
+    getRandomPosition() {
+        const x = this.getRandomNumberBetweenRange(0, this.rowsNumber - 1);
+        const y = this.getRandomNumberBetweenRange(0, this.columnsNumber - 1);
+        return {x, y};
+    }
+
+    getRandomCell() {
+        const {x, y} = this.getRandomPosition();
+        return this.map[y][x];
+    }
+
+    getAvailableRandomCell(iteration = 0) {
+        const cell = this.getRandomCell();
+
+        if (iteration > (this.rowsNumber * this.columnsNumber)) {
+            throw new Error("Plus aucune case disponible");
+        }
+
+        if (cell.wall || cell.weapon || cell.player) {
+            iteration++;
+            return this.getAvailableRandomCell(iteration);
+        }
+        return cell;
+    }
+
+    generateWeapons() {
+        for (let i = 0; i < this.weaponsCount; i++) {
+            const cell = this.getAvailableRandomCell();
+            this.map[cell.y][cell.x].weapon = true;
+        }
+    }
+
+    placePlayer(player) {
+        const cell = this.getAvailableRandomCell();
+        player.position = {x: cell.x, y: cell.y};
+        this.map[cell.y][cell.x].player = player;
+
+        /*
+        if (cell.y-1 !== -1 || 
+            cell.y+1 !== squareMap.columnsNumber-1 ||
+            cell.x-1 !== -1 ||
+            cell.x+1 !== squareMap.rowsNumber-1) {
+                console.log(map[cell.y - 1][cell.x]);
+                console.log(map[cell.y + 1][cell.x]);
+                console.log(map[cell.y][cell.x + 1]);
+                console.log(map[cell.y][cell.x - 1]);
+        }
+        */
+    }
+
+    checkSiblingsCells() {
+        //const cell = players[0].position;
+        //let cellOccupied = [];
+
+        console.log(this.map);
+
+        players.forEach((player, i) => {
+            //console.log(players);
+            const playerPosY = player.position.y;
+            const playerPosX = player.position.x;
+
+            //console.log(!!(map[playerPosY][playerPosX].player));
+            console.log(this.map[playerPosY][playerPosX]);
+
+            console.log(this.rowsNumber);
+            
+            if (playerPosY === (this.rowsNumber - 1) && playerPosX === 0 || 
+                playerPosY === (this.rowsNumber - 1) && playerPosX === (this.columnsNumber - 1) || 
+                playerPosY === 0 && playerPosX === 0 || 
+                playerPosY === 0 && playerPosX === (this.columnsNumber - 1)) {
+                    console.log("Player in square end");
+            } else if (
+                playerPosY === 10 && playerPosX > 0 && playerPosX < (this.columnsNumber - 1) || 
+                playerPosX === 0 && playerPosY > 0 && playerPosY < (this.rowsNumber - 1) || 
+                playerPosY === 0 && playerPosX > 0 && playerPosX < (this.columnsNumber - 1) || 
+                playerPosX === 10 && playerPosY > 0 && playerPosY < (this.rowsNumber - 1)) {
+                    console.log("Player in outlines cells");
+            } else if (playerPosY < (this.rowsNumber - 1) && playerPosY > 0 && playerPosX < (this.columnsNumber - 1)  && playerPosX > 0) {
+                console.log("Player in inner cells");
+            }
+
+            if (
+                playerPosY <= (this.rowsNumber - 1) && 
+                playerPosY >= 0 && 
+                playerPosX <= (this.columnsNumber - 1) && 
+                playerPosX >= 0 &&
+                playerPosY + 1 <= (this.rowsNumber - 1) &&
+                playerPosY - 1 >= 0 &&
+                playerPosX + 1 <= (this.columnsNumber - 1) &&
+                playerPosX - 1 >= 0) {
+                console.log(this.map[playerPosY - 1][playerPosX]);
+                console.log(this.map[playerPosY + 1][playerPosX]);
+                console.log(this.map[playerPosY][playerPosX + 1]);
+                console.log(this.map[playerPosY][playerPosX - 1]);
+                console.log(this.map[playerPosY - 1][playerPosX - 1]);
+                console.log(this.map[playerPosY - 1][playerPosX + 1]);
+                console.log(this.map[playerPosY + 1][playerPosX - 1]);
+                console.log(this.map[playerPosY + 1][playerPosX + 1]);
+            }
+            
+            /*
+            if (playerPosY === 10 && playerPosX === 0) {          // Square end
+                console.log(this.map[playerPosY - 1][playerPosX]);
+                console.log(this.map[playerPosY][playerPosX + 1]);
+                console.log(this.map[playerPosY - 1][playerPosX + 1]);
+                
+                if (!!(this.map[playerPosY - 1][playerPosX].player) || !!(this.map[playerPosY][playerPosX + 1].player) || !!(this.map[playerPosY - 1][playerPosX + 1]).player) {
+                    console.log('Cellule voisine occupée');
+                }
+                
+            } else if (playerPosY === 10 && playerPosX === 10) {  // Square end
+                console.log(this.map[playerPosY - 1][playerPosX]);
+                console.log(this.map[playerPosY - 1][playerPosX - 1]);
+                console.log(this.map[playerPosY][playerPosX - 1]);
+            } else if (playerPosY === 0 && playerPosX === 0) {    // Square end
+                console.log(this.map[playerPosY + 1][playerPosX]);
+                console.log(this.map[playerPosY][playerPosX + 1]);
+                console.log(this.map[playerPosY + 1][playerPosX + 1]);
+            } else if (playerPosY === 0 && playerPosX === 10) {   // Square end
+                console.log(this.map[playerPosY + 1][playerPosX]);
+                console.log(this.map[playerPosY][playerPosX - 1]);
+                console.log(this.map[playerPosY + 1][playerPosX - 1]);
+            } else if (playerPosY === 10 && playerPosX > 0 && playerPosX < 10) { // Outlines
+                console.log(this.map[playerPosY][playerPosX + 1]);
+                console.log(this.map[playerPosY][playerPosX - 1]);
+                console.log(this.map[playerPosY - 1][playerPosX]);
+                console.log(this.map[playerPosY - 1][playerPosX + 1]);
+                console.log(this.map[playerPosY - 1][playerPosX - 1]);
+            } else if (playerPosX === 0 && playerPosY > 0 && playerPosY < 10) { // Outlines
+                console.log(this.map[playerPosY][playerPosX + 1]);
+                console.log(this.map[playerPosY - 1][playerPosX]);
+                console.log(this.map[playerPosY - 1][playerPosX + 1]);
+                console.log(this.map[playerPosY + 1][playerPosX + 1]);
+                console.log(this.map[playerPosY + 1][playerPosX]);
+            } else if (playerPosY === 0 && playerPosX > 0 && playerPosX < 10) { // Outlines
+                console.log(this.map[playerPosY][playerPosX + 1]);
+                console.log(this.map[playerPosY][playerPosX - 1]);
+                console.log(this.map[playerPosY + 1][playerPosX]);
+                console.log(this.map[playerPosY + 1][playerPosX + 1]);
+                console.log(this.map[playerPosY + 1][playerPosX - 1]);
+            } else if (playerPosX === 10 && playerPosY > 0 && playerPosY < 10) { // Outlines
+                console.log(this.map[playerPosY][playerPosX - 1]);
+                console.log(this.map[playerPosY - 1][playerPosX]);
+                console.log(this.map[playerPosY + 1][playerPosX]);
+                console.log(this.map[playerPosY + 1][playerPosX - 1]);
+                console.log(this.map[playerPosY - 1][playerPosX - 1]);
+            } else if (playerPosY < 10 && playerPosY > 0 && playerPosX < 10  && playerPosX > 0) { // Inner cells
+                console.log(this.map[playerPosY - 1][playerPosX]);
+                console.log(this.map[playerPosY + 1][playerPosX]);
+                console.log(this.map[playerPosY][playerPosX + 1]);
+                console.log(this.map[playerPosY][playerPosX - 1]);
+                console.log(this.map[playerPosY - 1][playerPosX - 1]);
+                console.log(this.map[playerPosY - 1][playerPosX + 1]);
+                console.log(this.map[playerPosY + 1][playerPosX - 1]);
+                console.log(this.map[playerPosY + 1][playerPosX + 1]);
+
+                if (
+                    !!(this.map[playerPosY - 1][playerPosX].player) || 
+                    !!(this.map[playerPosY][playerPosX + 1].player) || 
+                    !!(this.map[playerPosY - 1][playerPosX + 1]).player || 
+                    !!(this.map[playerPosY + 1][playerPosX]).player ||
+                    !!(this.map[playerPosY][playerPosX - 1].player) ||
+                    !!(this.map[playerPosY - 1][playerPosX - 1].player) ||
+                    !!(this.map[playerPosY + 1][playerPosX - 1].player) ||
+                    !!(this.map[playerPosY + 1][playerPosX + 1].player)) {
+                    console.log('Cellule voisine occupée');
+                }
+
+            }
+            */
+        });
+    }
+
+    printMap() {
+        const tbody = document.createElement("tbody");
+        this.map.forEach((row, indexY) => {
+            const tr = document.createElement("tr");
+            tr.dataset.y = indexY;
+            tbody.appendChild(tr);
+            row.forEach((cell, indexX) => {
+                const td = document.createElement("td");
+                td.dataset.x = indexX;
+                td.dataset.yx = `${indexY}-${indexX}`;
+                
+                if (cell.wall) {
+                    td.classList.add("wall");
+                }
+                
+                if (cell.weapon) {
+                    td.classList.add("weapon");
+                }
+
+                if (cell.player) {
+                    td.classList.add("player");
+                }
+                
+                tr.appendChild(td);
+            });
+        });
+        this.el.appendChild(tbody);
+    }
+
+    getRandomNumberBetweenRange(min, max) { 
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
 
@@ -22,160 +258,11 @@ const squareMap = new Map({
     columnsNumber: 11,
     rowsNumber: 11,
     percentageDisabledCells: 10,
-    weaponsCount : 4
+    weaponsCount : 4,
+    el: document.querySelector("table#myTable"),
 });
 
-function getRandomNumberBetweenRange(min, max) { 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
-function generateMap() {
-    let map = [];
-    for (let y = 0; y < squareMap.rowsNumber; y++) {
-        map[y] = [];
-        for (let x = 0; x < squareMap.columnsNumber; x++) {
-            map[y][x] = {x, y};
-        }
-    }
-    return map;
-}
-
-function getRandomPosition() {
-    const x = getRandomNumberBetweenRange(0, squareMap.rowsNumber - 1);
-    const y = getRandomNumberBetweenRange(0, squareMap.columnsNumber - 1);
-    return {x, y};
-}
-
-function getRandomCell() {
-    const {x, y} = getRandomPosition();
-    return map[y][x];
-}
-
-function getAvailableRandomCell(iteration = 0) {
-    const cell = getRandomCell();
-
-    if (iteration > (squareMap.rowsNumber * squareMap.columnsNumber)) {
-        throw new Error("Plus aucune case disponible");
-    }
-
-    if (cell.wall || cell.weapon || cell.player) {
-        iteration++;
-        return getAvailableRandomCell(iteration);
-    }
-    return cell;
-}
-
-function generateWeapons(baseMap) {
-    const map = baseMap;
-    for (let i = 0; i < squareMap.weaponsCount; i++) {
-        const cell = getAvailableRandomCell();
-        map[cell.y][cell.x].weapon = true;
-    }
-    return map;
-}
-
-function generateWalls(baseMap) {
-    const map = baseMap;
-    const wallsToBuild = Math.floor((squareMap.rowsNumber * squareMap.columnsNumber) * (squareMap.percentageDisabledCells / 100));
-    for (let i = 0; i < wallsToBuild; i++) {
-        const cell = getAvailableRandomCell();
-        map[cell.y][cell.x].wall = true;
-    }
-    return map;
-}
-
-function placePlayer(player) {
-    const cell = getAvailableRandomCell();
-    player.position = {x: cell.x, y: cell.y};
-    map[cell.y][cell.x].player = player;
-
-    /*
-    if (cell.y-1 !== -1 || 
-        cell.y+1 !== squareMap.columnsNumber-1 ||
-        cell.x-1 !== -1 ||
-        cell.x+1 !== squareMap.rowsNumber-1) {
-            console.log(map[cell.y - 1][cell.x]);
-            console.log(map[cell.y + 1][cell.x]);
-            console.log(map[cell.y][cell.x + 1]);
-            console.log(map[cell.y][cell.x - 1]);
-    }
-    */
-}
-
-function checkSiblingsCells() {
-    const cell = players[0].position;
-
-    console.log(map);
-
-    players.forEach(function(player, i) {
-        //console.log(players);
-        const playerPosY = player.position.y;
-        const playerPosX = player.position.x;
-
-        console.log(map[playerPosY][playerPosX]);
-    });
-
-    /*
-    // create new function
-    if (cell.y < 10 && cell.x < 10 || cell.y > 0 && cell.x > 0) {
-        console.log(map[cell.y][cell.x]);
-        console.log(map[cell.y - 1][cell.x]);
-        console.log(map[cell.y + 1][cell.x]);
-        console.log(map[cell.y][cell.x + 1]);
-        console.log(map[cell.y][cell.x - 1]);
-    } else {
-        console.log("Hors map");
-    }
-    */
-    
-    //console.log(players[0].position.y);
-    //console.log(map[cell.y - 1][cell.x]);
-    //console.log(map[cell.y - 1][cell.x - 1]);
-    //console.log(map[cell.y + 1][cell.x]);
-    //console.log(map[cell.y + 1][cell.x + 1]);
-    //console.log(map[cell.y][cell.x + 1]);
-    //console.log(map[cell.y][cell.x - 1]);
-}
-
-function printMap(map) {
-    const table = document.querySelector("table");
-    const tbody = document.createElement("tbody");
-    map.forEach((row, indexY) => {
-        const tr = document.createElement("tr");
-        tr.dataset.y = indexY;
-        tbody.appendChild(tr);
-        row.forEach((cell, indexX) => {
-            const td = document.createElement("td");
-            td.dataset.x = indexX;
-            td.dataset.yx = `${indexY}-${indexX}`;
-            
-            if (cell.wall) {
-                td.classList.add("wall");
-            }
-            
-            if (cell.weapon) {
-                td.classList.add("weapon");
-            }
-
-            if (cell.player) {
-                td.classList.add("player");
-            }
-            
-            tr.appendChild(td);
-        });
-    });
-    table.appendChild(tbody);
-}
-
-let map = generateMap();
-map = generateWalls(map);
-map = generateWeapons(map);
-
-placePlayer(players[0]);
-placePlayer(players[1]);
-
-checkSiblingsCells();
-printMap(map);
 
 //squareMap.getRandomCell();
 //squareMap.printMap();
--- players.js
+++ players.js
@@ -1,15 +1,16 @@
 class Player {
-    constructor(name, weapon, picture, health, position) {
+    constructor(name, weapon, picture, health, shield, position) {
         this.name = name;
         this.weapon = weapon;
         this.picture = picture;
         this.health = health;
+        this.shield = shield
         this.position = position;
     }
 }
 
-const iop = new Player("Iop", goultard, "./sprites/iop.png", 100, "");
-const steamer = new Player("Steamer", raziel, "./sprites/steamer.png", 100, "");
+const iop = new Player("Iop", goultard, "./sprites/iop.png", 100, 0, "");
+const steamer = new Player("Steamer", raziel, "./sprites/steamer.png", 100, 0, "");
 
 const players = [iop, steamer];
 //console.log(players)
